17e1eb3f2b35631480eb414773c6db1b15a2aaec
Fotache, Dragos <Dragos.Fotache@awinta.de>
2016-10-11 10:30:00 +0300

    .

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
 
-import { AppComponent } from './app.component';
+const platform = platformBrowserDynamic();
 
-bootstrap(AppComponent);
+platform.bootstrapModule(AppModule);
